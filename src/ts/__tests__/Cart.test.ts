fed448619ac8d4009a538047a6c245d90088ff7d
Yulia Ermolaeva <168757304+yuliakorsak@users.noreply.github.com>
2024-10-30 16:00:52 +0300

    Updated Cart.ts, Cart.test.ts

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -1,5 +1,7 @@
 import Cart from '../service/Cart';
+import Book from '../domain/Book';
 import Movie from '../domain/Movie';
+import MusicAlbum from '../domain/MusicAlbum';
 
 test('new card should be empty', () => {
   const cart = new Cart();
@@ -8,8 +10,40 @@ test('new card should be empty', () => {
 });
 
 test('adding to cart', () => {
-  const movie = new Movie(3002, 'Мстители', 900, 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137);
   const cart = new Cart();
+  const movie = new Movie(1015, 'Мстители', 900, 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137);
   cart.add(movie);
   expect(cart.items[0]).toBe(movie);
+});
+
+test('get total', () => {
+  const cart = new Cart();
+  cart.add(new Book(1001, 'War and Peace', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  cart.add(new Movie(1015, 'Мстители', 900, 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137));
+  const total = cart.total();
+  expect(total).toBe(3800);
+});
+
+test('get total with discount', () => {
+  const cart = new Cart();
+  cart.add(new Book(1001, 'War and Peace', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  cart.add(new Movie(1015, 'Мстители', 900, 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137));
+  const discounted = cart.totalDiscounted(20);
+  expect(discounted).toBeCloseTo(3040);
+});
+
+test('remove from cart', () => {
+  const cart = new Cart();
+  cart.add(new Book(1001, 'War and Peace', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  cart.add(new Movie(1015, 'Мстители', 900, 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137));
+
+  const expected = new Cart;
+  expected.add(cart.items[0]);
+  expected.add(cart.items[2]);
+
+  cart.remove(1008);
+  expect(cart).toEqual(expected);
 });
--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -8,6 +8,20 @@ export default class Cart {
     }
 
     get items(): Buyable[] {
-        return [...this._items]; 
+        return [...this._items];
+    }
+
+    total(): number {
+        return this.items.reduce((sum, currentItem) => sum + currentItem.price, 0);
+    }
+
+    totalDiscounted(discountPercent: number): number {
+        const total = this.total();
+        return total - total * discountPercent * 0.01;
+    }
+
+    remove(id: number): void {
+        const index = this.items.findIndex(item => item.id === id);
+        this._items.splice(index, 1);
     }
 }
